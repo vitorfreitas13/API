69a85007c04a43a776c6e8e10ec18cda12ea5d1e
Vitor Freitas <dev.vitorfreitas13@gmail.com>
2026-02-04 17:55:24 -0300

    inicio de back end

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+const users = []
+
+app.post('/usuarios', (req, res) =>{
+    
+    users.push(req.body)
+
+    res.status(201).json(req.body)
+})
+
+app.get('/usuarios', (req, res)=>{
+    res.status(200).json(users)
+})
+
+
+app.listen(3000)
